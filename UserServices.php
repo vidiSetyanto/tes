52eb3a111a52a95bd649503053248fd00bb01289
Vidi <dek.vidi@gmail.com>
2016-04-19 01:27:08 +0700

    :update

--- UserServices.php
+++ UserServices.php
@@ -5,25 +5,44 @@ header("Content-Type: application/json; charset=UTF-8");
 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
-$name = $request->name;
+$_serviceName = $request->serviceName;
+$_datas = $request->datas;
 
 $conn = new mysqli("mysql.idhostinger.com", "u532537586_vidi", "database2", "u532537586_vidi");
+if($_serviceName=="RetrieveTransaction")
+{
+	$name = $request->name;
+	$result = $conn->query("SELECT t.Date Date,
+	t.amount Amount,
+	t.desc Description
+	FROM Customer c join w0_transaction t on c.ID=t.cust_ID
+	where c.Name = '".$name."' order by Date desc");
 
-$result = $conn->query("SELECT t.Date Date,
-t.amount Amount,
-t.desc Description
- FROM Customer c join w0_transaction t on c.ID=t.cust_ID
- where c.Name = '".$name."'");
-
-$outp = "";
-while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-    if ($outp != "") {$outp .= ",";}
-    $outp .= '{"Date":"'  . $rs["Date"] . '",';
-	$outp .= '"Description":"'  . $rs["Description"] . '",';
-    $outp .= '"Amount":"'. $rs["Amount"]     . '"}'; 
+	$outp = "";
+	while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+		if ($outp != "") {$outp .= ",";}
+		$outp .= '{"Date":"'  . $rs["Date"] . '",';
+		$outp .= '"Description":"'  . $rs["Description"] . '",';
+		$outp .= '"Amount":"'. $rs["Amount"]     . '"}'; 
+		}
+		$outp ='{"records":['.$outp.']}';
+		$conn->close();
 }
-$outp ='{"records":['.$outp.']}';
-$conn->close();
+else if( $_serviceName == "AddTransaction" )
+{
+	if($_datas->pwd != "aslk")
+	{
+		trigger_error("Not authorized");		
+		$conn->close();
+		return;
+	}
+	$_name = $request->name;
+	$result = $conn->query("INSERT INTO w0_transaction( `amount`, `cust_ID`, `desc`) 
+	VALUES (".$_datas->amount.",".$_datas->name->ID.",'".$_datas->description."')");
 
-echo($outp);
+	
+		$conn->close();
+}
+if($outp)
+	echo($outp);
 ?>
--- modules/ctrl/service.js
+++ modules/ctrl/service.js
@@ -3,8 +3,12 @@ app.controller('customersCtrl', function($scope, $http) {
 
 $scope.GetNames = GetNames;
 $scope.SelectName = SelectName;
-
+$scope.Insert = Insert;
 $scope.$watch( 'selected',SelectedChanged );
+$scope.selected = {Name:""};
+$scope.Pwd='';
+
+$scope.Show = Show;
 
 function GetNames()
 {
@@ -23,7 +27,7 @@ function SelectName( name )
  headers: {
    'Content-Type': 'application/json; charset=UTF-8'
  },
- data: { name: name }
+ data: { serviceName:"RetrieveTransaction", name: name }
 }
 
 $http(req).then(function (response) {
@@ -33,7 +37,7 @@ $http(req).then(function (response) {
    for(var i=0;i<records.length;i++)
 	   total+=Number( records[ i ].Amount );
    $scope.Total = total;
-   $scope.List = records;});
+   $scope.List = records;}).catch(Error);
 }
 
 function SelectedChanged( val )
@@ -44,6 +48,46 @@ function SelectedChanged( val )
 	}
 }
 
+function Insert()
+{
+	console.log($scope.selected,$scope.Amount, $scope.Description);
+	if( !$scope.selected.ID )
+	{
+		Error( "User " + $scope.selected + " Not Found !!" );
+		return;
+	}
+	if( !$scope.Amount )
+	{
+		Error( "Amount cant be null !!" );
+		return;
+	}
+	var req = {
+		method: 'POST',
+		url: 'http://tpulsa.com/UserServices.php',
+		headers: {
+			'Content-Type': 'application/json; charset=UTF-8'
+			},
+			data: { serviceName: "AddTransaction", datas:{name:$scope.selected, amount:$scope.Amount, description: $scope.Description, pwd:$scope.Pwd} }
+			}
+
+$http(req).then(function (response) {
+   console.log(response);
+   $scope.Amount="";
+   $scope.Description="";
+   SelectedChanged($scope.selected);
+   alert( "inserted" );
+   }).catch(Error);
+}
+
+function Error(response)
+{
+	alert(response.toString());
+}
+
+function Show()
+{
+	return $scope.Pwd.length==4;
+}
 });
 app.controller('TypeaheadCtrl', function($scope, $http) {
 
